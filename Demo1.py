8844c6f5870a0c44e856d0935be6b86bf42b2622
Dale Young <1842163947@qq.com>
2024-12-17 02:16:44 +0800

    second comimit

--- Demo1.py
+++ Demo1.py
@@ -18,6 +18,7 @@ class AUTODL:
     def __init__(self,git,you):
         self.git = git
         self.you = you
+        self.add = git + you
 # Neural Network Model for Q-Learning
 # This model will approximate the Q-Values for each possible action(node to pick next)
 # This network has three fully connected layers with ReLU activations
