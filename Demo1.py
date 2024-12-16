35068a9ea2a903173f1b2fd4aa8e0c96f469a6e2
Dale Young <1842163947@qq.com>
2024-12-17 01:44:41 +0800

    second commit

--- Demo1.py
+++ Demo1.py
@@ -14,7 +14,9 @@ class TestNode(object):
         self.cost = cost
         self.faultIsolationArray = faultIsolationArray
 
-class
+class AUTODL:
+    def __init__(self,git):
+        self.git = git
 # Neural Network Model for Q-Learning
 # This model will approximate the Q-Values for each possible action(node to pick next)
 # This network has three fully connected layers with ReLU activations
