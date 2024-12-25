b2fceb111b31377ce2259d3b13b89cfe226f6ad8
Dale Young <1842163947@qq.com>
2024-12-25 14:14:42 +0800

    12/25 commit

--- GMM.py
+++ GMM.py
@@ -22,9 +22,12 @@ def gmm_fault_separation(train_data, test_data, test_label, n_classes):
     category_accuracy = []
     for cluster in range(n_classes):
         cluster_indexes = np.where(y_pred == cluster)[0]
-        true_labels = test_label[cluster_indexes]
-        most_common_label = mode(true_labels)[0][0]
-        accuracy = np.sum(true_labels == most_common_label) / len(true_labels)
+        if len(cluster_indexes) > 0:
+            true_labels = [test_label[cluster_index] for cluster_index in cluster_indexes]
+            most_common_label = mode(true_labels)[0][0]
+            accuracy = np.sum(true_labels == most_common_label) / len(true_labels)
+        else:
+            accuracy = 0
         category_accuracy.append(accuracy)
     return category_accuracy
 
@@ -32,7 +35,8 @@ def preprocessing(file):
     data_file = io.loadmat(file)
     train_data = data_file["train_data"]
     test_data = data_file["test_data"]
-    test_label = data_file["test_label"]
+    test_label = data_file["test_y"]
+    test_label = [np.argmax(test_y) for test_y in test_label]
 
     standard_scaler = StandardScaler()
     train_data = standard_scaler.fit_transform(train_data)
@@ -48,13 +52,10 @@ def node_accuracy_dic_generate(path):
     files = os.listdir(path)
     for file in files:
         sub_path = os.path.join(path, file)
-        sub_files = os.listdir(sub_path)
-        for sub_file in sub_files:
-            if os.path.isfile(sub_file):
-                file_path = os.path.join(sub_path, sub_file)
-                train_data, test_data, test_label = preprocessing(file_path)
-                accuracy_this_node = gmm_fault_separation(train_data, test_data, test_label, n_classes=13)
-                accuracy_dic[sub_file[:-4]] = accuracy_this_node
+        if os.path.isfile(sub_path):
+            train_data, test_data, test_label = preprocessing(sub_path)
+            accuracy_this_node = gmm_fault_separation(train_data, test_data, test_label, n_classes=13)
+            accuracy_dic[file[:-4]] = accuracy_this_node
     return accuracy_dic
 
 
